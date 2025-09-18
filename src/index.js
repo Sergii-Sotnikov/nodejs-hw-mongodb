f405f96ad4d5ed528229dcc63607325c633c53d0
SotnikovSS <sotnikovss90@gmail.com>
2025-09-18 23:07:28 +0200

    go

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { initMongoConnection } from "./db/initMongoDB.js";
+import { setupServer } from "./server.js";
+
+
+const bootstrap = async () => {
+  await initMongoConnection()
+  await setupServer().catch((error) => console.error(error));
+};
+
+bootstrap();
---
+++ src/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export async function setupServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  //Логування запитів
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+
+    res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  //запуск сервера
+  try {
+    app.listen(PORT, (error) => {
+      if (error) throw error;
+    });
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
