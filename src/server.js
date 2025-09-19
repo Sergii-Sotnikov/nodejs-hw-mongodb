6c5e84e93ccd2daef8a04d90fc24ba7f36f22687
SotnikovSS <sotnikovss90@gmail.com>
2025-09-19 15:29:15 +0200

    go

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ export async function setupServer() {
 
     res.status(200).json({
       status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
+      message: `Successfully found contact with id: ${contactId}!`,
       data: contact,
     });
   });
