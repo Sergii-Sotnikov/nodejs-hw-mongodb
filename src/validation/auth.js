a914c45a1ae20a6537add60652be1a2786906a50
SotnikovSS <sotnikovss90@gmail.com>
2025-10-09 18:56:22 +0200

    go

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -33,9 +33,9 @@ export const requestResetEmailSchema = Joi.object({
 });
 
 export const resetPasswordSchema = Joi.object({
-  password: Joi.string().required().messages({
+  token: Joi.string().required(),
+    password: Joi.string().required().messages({
     'string.min': 'Password should have at least {#limit} characters',
     'any.required': 'Password is required',
   }),
-  token: Joi.string().required(),
 });
